0ba7d43926ae617e67551cde464df5a901cd7000
Carlos Bezerra <cbezerraneto@gmail.com>
2023-03-17 11:56:23 -0300

    add typing effect

--- cogs/ecoia.py
+++ cogs/ecoia.py
@@ -24,23 +24,24 @@ class Ecoia(commands.Cog):
     @commands.Cog.listener()
     async def on_message(self, message):
         """Start the conversation with AI"""
-        if message.author.bot:
-            print('OPs, u are a bot xD')
-        else:
-            if 'eco' in message.content:
-                #async with ctx.channel.typing():
-                GPT_TOKEN = os.getenv("GPT_TOKEN")
-                query = message.content.replace("eco","")
-                response = openai.Completion.create(
-                    api_key = GPT_TOKEN,
-                    model="text-davinci-003",
-                    prompt=query,
-                    temperature=0.5,
-                    max_tokens=2000,
-                    top_p=0.3,
-                    frequency_penalty=0.5,
-                    presence_penalty=0.0
-                )
+        msg = message.content.lower()
+        eco = "eco"
+
+        if not message.author.bot:
+            if eco in msg:
+                async with message.channel.typing():
+                    GPT_TOKEN = os.getenv("GPT_TOKEN")
+                    query = message.content #.replace("eco","")
+                    response = openai.Completion.create(
+                        api_key = GPT_TOKEN,
+                        model="text-davinci-003",
+                        prompt=query,
+                        temperature=0.5,
+                        max_tokens=2000,
+                        top_p=0.3,
+                        frequency_penalty=0.5,
+                        presence_penalty=0.0
+                    )
                 await message.channel.send(content=response['choices'][0]['text'].replace(str(query), ""))
                 # print (query)
 
