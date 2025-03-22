2b02d6bbff5fa24aa63a5a004bcc5ac81068c0f6
BezerraC <cbezerraneto@gmail.com>
2025-03-22 19:03:01 -0300

    feat: update news

--- cogs/news.py
+++ cogs/news.py
@@ -15,10 +15,8 @@ class News(commands.Cog):
         embed.set_thumbnail(url=ctx.bot.user.avatar_url)
 
         description = f"Check the latest updates\n\n"
-       
-        description += "**Invite Command** \nNow you can generate an instant server invite.\n\n"
 
-        description += "**Eco I.A** \nChange the max_token to `3000`, now he can process more lines in your answer\nAdd type effect, wen he staying processing a answer you can view the time in `typing`\n\n"
+        description += "**Eco I.A** \nNew agent to use ia\n\n"
         
         description += "Developed by Eco#0745 \n\n"
         description += "*For more information call [developer](https://discord.gg/HahR7qjQ7s)*"
